4102c10e9e44f35fd1fdbb00586980233a2d1b81
higaki-codesandbox <garoumare@gmail.com>
2023-01-11 13:31:44 +0000

    Update 6 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./styles.css";
 import { InlineStyle } from "./components/inlineStyle";
+import { CssModules } from "./components/CssModules";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
